13b6d462ea92e88d07478333eaab106e1cc8c86c
Adriano Santos <sleipnir@bsd.com.br>
2023-09-16 16:21:47 -0300

    Update protobuf

--- spawn-rs/src/spawn.rs
+++ spawn-rs/src/spawn.rs
@@ -1,18 +1,34 @@
 use crate::actor::ActorDefinition;
+use crate::eigr::spawn::{ActorId, ActorInvocation, ActorInvocationResponse, Context};
 use crate::handler::actor_router::Handler;
 
+use prost::Message;
 use rocket::post;
 
 use rocket::data::{Data, ToByteUnit};
 use rocket::State;
+
+use std::io;
+use std::io::Cursor;
 
 #[post(
     "/api/v1/actors/actions",
     format = "application/octet-stream",
     data = "<data>"
 )]
-fn handle(data: Data<'_>, _handler: &State<Handler>) -> &'static str {
-    "Hello, world!"
+async fn handle(data: Data<'_>, _handler: &State<Handler>) -> io::Result<Vec<u8>> {
+    let bytes = data.open(2048.megabytes()).into_bytes().await?;
+
+    let buffer = bytes.into_inner();
+    let request: ActorInvocation = ActorInvocation::decode(&mut Cursor::new(buffer)).unwrap();
+    let actor: ActorId = request.actor.unwrap();
+    let action: String = request.action_name;
+    let ctx: Context = request.current_context.unwrap();
+
+    let mut buf: Vec<u8> = Vec::new();
+    let default_response = ActorInvocationResponse::default();
+    default_response.encode(&mut buf).unwrap();
+    Ok(buf)
 }
 
 #[derive()]
